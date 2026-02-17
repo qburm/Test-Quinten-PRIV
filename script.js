d80b184289ea19aacdabf6e48748218aa83ad930
qburm <qburm@rb-media.nl>
2026-02-17 11:32:55 +0100

    Create a new file script.js with simple JavaScript code for practice

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple JavaScript code for practice
+
+console.log('Hello, World!');
+// This script prints the current date and time in UTC format
+const currentDateTime = new Date();
+console.log(`Current Date and Time (UTC): ${currentDateTime.toUTCString()}`);
